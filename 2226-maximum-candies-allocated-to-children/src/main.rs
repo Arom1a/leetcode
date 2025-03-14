ea3f36469fc31aa25edc6cacf0ec5f5ce95e347a
Aromia <f2940015504@gmail.com>
2025-03-14 11:24:00 +0800

    2226 done

---
+++ 2226-maximum-candies-allocated-to-children/src/main.rs
@@ -0,0 +1,57 @@
+fn check_num(candies: &Vec<i32>, k: i64, n: i32) -> bool {
+    let mut child_num = 0;
+    for i in candies {
+        child_num += (*i / n) as i64;
+        if child_num >= k {
+            return true;
+        }
+    }
+
+    false
+}
+
+// Solution 1: standard binary search
+// pub fn maximum_candies(candies: Vec<i32>, k: i64) -> i32 {
+//     let sum: i64 = candies.iter().map(|&x| x as i64).sum();
+//     let max = (sum / k) as i32;
+//
+//     let mut l = 0;
+//     let mut r = max + 1;
+//     while l != r {
+//         let mid = l + (r - l + 1) / 2;
+//         if check_num(&candies, k, mid) {
+//             l = mid;
+//         } else {
+//             r = mid - 1;
+//         }
+//     }
+//
+//     l
+// }
+
+// Solution 2: [meguru method](https://x.com/meguru_comp/status/697008509376835584)
+pub fn maximum_candies(candies: Vec<i32>, k: i64) -> i32 {
+    let sum: i64 = candies.iter().map(|&x| x as i64).sum();
+    let max = (sum / k) as i32;
+
+    let mut l = 0;
+    let mut r = max + 1;
+
+    while (r - l).abs() > 1 {
+        let mid = (l + r) / 2;
+        if check_num(&candies, k, mid) {
+            l = mid;
+        } else {
+            r = mid;
+        }
+    }
+
+    l
+}
+
+fn main() {
+    assert_eq!(maximum_candies(vec![5, 8, 6], 3), 5);
+    assert_eq!(maximum_candies(vec![2, 5], 11), 0);
+    assert_eq!(maximum_candies(vec![4, 7, 5], 16), 1);
+    println!("All passed!");
+}
