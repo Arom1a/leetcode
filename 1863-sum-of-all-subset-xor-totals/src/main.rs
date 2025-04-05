f73ca5f8c5674de77e08edf297b56607da07ba90
Aromia <f2940015504@gmail.com>
2025-04-05 23:26:12 +0800

    1863 done

---
+++ 1863-sum-of-all-subset-xor-totals/src/main.rs
@@ -0,0 +1,22 @@
+fn help(n: usize, nums: &Vec<i32>, idx: usize, accu: i32, ans: &mut i32) {
+    if idx == n {
+        *ans += accu;
+        return;
+    }
+
+    help(n, nums, idx + 1, accu, ans);
+    help(n, nums, idx + 1, accu ^ nums[idx], ans);
+}
+
+pub fn subset_xor_sum(nums: Vec<i32>) -> i32 {
+    let mut ans = 0;
+    help(nums.len(), &nums, 0, 0, &mut ans);
+    ans
+}
+
+fn main() {
+    assert_eq!(subset_xor_sum(vec![1, 3]), 6);
+    assert_eq!(subset_xor_sum(vec![5, 1, 6]), 28);
+    assert_eq!(subset_xor_sum(vec![3, 4, 5, 6, 7, 8,]), 480);
+    println!("All passed!");
+}
