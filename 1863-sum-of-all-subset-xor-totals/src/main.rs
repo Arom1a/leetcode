707fca881a7ef664d3bf4349c94aa36a25146ed6
Aromia <f2940015504@gmail.com>
2025-04-05 23:37:35 +0800

    1863 improved

--- 1863-sum-of-all-subset-xor-totals/src/main.rs
+++ 1863-sum-of-all-subset-xor-totals/src/main.rs
@@ -1,17 +1,27 @@
-fn help(n: usize, nums: &Vec<i32>, idx: usize, accu: i32, ans: &mut i32) {
-    if idx == n {
-        *ans += accu;
-        return;
-    }
+// Solution 1
+// fn help(n: usize, nums: &Vec<i32>, idx: usize, accu: i32, ans: &mut i32) {
+//     if idx == n {
+//         *ans += accu;
+//         return;
+//     }
+//
+//     help(n, nums, idx + 1, accu, ans);
+//     help(n, nums, idx + 1, accu ^ nums[idx], ans);
+// }
+//
+// pub fn subset_xor_sum(nums: Vec<i32>) -> i32 {
+//     let mut ans = 0;
+//     help(nums.len(), &nums, 0, 0, &mut ans);
+//     ans
+// }
 
-    help(n, nums, idx + 1, accu, ans);
-    help(n, nums, idx + 1, accu ^ nums[idx], ans);
-}
-
+// Solution 2
 pub fn subset_xor_sum(nums: Vec<i32>) -> i32 {
     let mut ans = 0;
-    help(nums.len(), &nums, 0, 0, &mut ans);
-    ans
+    for i in &nums {
+        ans |= i;
+    }
+    ans << (nums.len() - 1)
 }
 
 fn main() {
