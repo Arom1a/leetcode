43637c30dec3c320ef041aee1cab66f742db6d93
Aromia <f2940015504@gmail.com>
2025-05-02 23:55:49 +0800

    0838 done

---
+++ 0838-push-dominoes/src/main.rs
@@ -0,0 +1,37 @@
+// Solution 1: TODO
+pub fn push_dominoes(dominoes: String) -> String {
+    let mut ds = dominoes.into_bytes();
+    let mut l = 0;
+    let mut r = 0;
+    while l + 1 != ds.len() {
+        while r + 1 < ds.len() && ds[r] == b'.' {
+            r += 1;
+        }
+        match (ds[l], ds[r]) {
+            (b'R', b'L') => {
+                let h = (r - l + 1) / 2;
+                &mut ds[l..l + h].fill(b'R');
+                &mut ds[r - h + 1..=r].fill(b'L');
+            }
+            (b'R', _) => {
+                &mut ds[l..=r].fill(b'R');
+            }
+            (_, b'L') => {
+                &mut ds[l..=r].fill(b'L');
+            }
+            _ => {}
+        }
+        l = r;
+        r += 1;
+    }
+    String::from_utf8(ds).unwrap()
+}
+
+fn main() {
+    assert_eq!(push_dominoes("RR.L".to_string()), "RR.L".to_string());
+    assert_eq!(
+        push_dominoes(".L.R...LR..L..".to_string()),
+        "LL.RR.LLRRLL..".to_string()
+    );
+    println!("All passed!");
+}
